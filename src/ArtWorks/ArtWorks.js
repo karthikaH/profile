706114594800e64e42bb517e17515cee5d7ec6d1
karthikah <hariharan.karthika93@gmail.com>
2019-11-10 12:45:26 +0530

    created a data file for all the art works and their details

--- src/ArtWorks/ArtWorks.js
+++ src/ArtWorks/ArtWorks.js
@@ -2,16 +2,9 @@ import React from "react";
 import {Link} from "@reach/router";
 
 import './ArtWorks.scss'
-import sunset from "../assets/gallaryImages/sunset.jpg";
-import coffeeGirl from "../assets/gallaryImages/coffeeGirl.jpeg";
-import dance from "../assets/gallaryImages/dance.jpeg";
-import tribalArt from "../assets/gallaryImages/tribalArt.jpeg";
-import ride from "../assets/gallaryImages/ride.jpeg";
-import rain from "../assets/gallaryImages/rain.jpeg";
-import rocket from "../assets/gallaryImages/rocket.jpg";
-import jumpingjack from "../assets/gallaryImages/jumpingjack.jpg";
-import krishna from "../assets/gallaryImages/krishna.jpg";
 import AboutPageFooter from "../AboutPageFooter/AboutPageFooter";
+
+import artData from './ArtWorksData';
 
 
 const ArtWorks = () => {
@@ -34,60 +27,17 @@ const ArtWorks = () => {
                 </Link>
             </div>
             <ul>
-            <li>
-				<figure className="figureStyles">
-					<img src={tribalArt} alt="tribal art"></img>
-					<figcaption className="captionStyles">Rise of the power within us against all odds!</figcaption>
-				</figure>
-			</li>
-			<li>
-				<figure className="figureStyles"> 
-					<img src={dance} alt="dance"></img>
-					<figcaption className="captionStyles">The Graceful Dancer!</figcaption>
-				</figure>
-			</li>
-			<li>
-				<figure className="figureStyles">
-					<img src={coffeeGirl} alt="coffee girl"></img>
-					<figcaption className="captionStyles">Enjoying a nice cup of coffee!</figcaption>
-				</figure>
-			</li>
-            <li>
-				<figure className="figureStyles">
-					<img src={rain} alt="rain"></img>
-					<figcaption className="captionStyles">A walk with a drizzle <span role='img' aria-label='rain'>🌧</span></figcaption>
-				</figure>
-			</li>
-            <li>
-				<figure className="figureStyles">
-					<img src={rocket} alt="rocket"></img>
-					<figcaption className="captionStyles">Let us ride to the moon!<span role='img' aria-label='rain'>🌔</span></figcaption>
-				</figure>
-			</li>
-            <li>
-				<figure className="figureStyles">
-					<img src={jumpingjack} alt="jumpingjack"></img>
-					<figcaption className="captionStyles">Let us create magic moments in our lives!<span role='img' aria-label='rain'>🎩</span></figcaption>
-				</figure>
-			</li>
-            <li>
-				<figure className="figureStyles">
-					<img src={krishna} alt="krishna"></img>
-					<figcaption className="captionStyles">The mischievous Lord Krishna</figcaption>
-				</figure>
-			</li>
-            <li>
-				<figure className="figureStyles">
-					<img src={sunset} alt="sunset"></img>
-					<figcaption className="captionStyles">A family kissed by the sun while saying goodbye to the day and welcoming the stary night.</figcaption>
-				</figure>
-			</li>
-            <li>
-				<figure className="figureStyles">
-					<img src={ride} alt="lets ride"></img>
-					<figcaption className="captionStyles">Let us take a breath of fresh air through a long ride <span role='img' aria-label='happy'>😀</span></figcaption>
-				</figure>
-			</li>
+				{artData.map(artDet => (
+					<li key={artDet.id}>
+					<figure className="figureStyles">
+						<img src={artDet.imgSrc} alt={artDet.alternativeText}></img>
+						<figcaption className="captionStyles">{artDet.caption}
+						<span role='img' aria-label='rain' className="heart">🧡</span>
+						<span className="heartCount">0</span>
+						</figcaption>
+					</figure>
+				</li>
+				))}
             </ul>
             <AboutPageFooter />
         </div>
---
+++ src/ArtWorks/ArtWorksData.js
@@ -0,0 +1,77 @@
+import sunset from "../assets/gallaryImages/sunset.jpg";
+import coffeeGirl from "../assets/gallaryImages/coffeeGirl.jpeg";
+import dance from "../assets/gallaryImages/dance.jpeg";
+import tribalArt from "../assets/gallaryImages/tribalArt.jpeg";
+import ride from "../assets/gallaryImages/ride.jpeg";
+import rain from "../assets/gallaryImages/rain.jpeg";
+import rocket from "../assets/gallaryImages/rocket.jpg";
+import jumpingjack from "../assets/gallaryImages/jumpingjack.jpg";
+import krishna from "../assets/gallaryImages/krishna.jpg";
+
+const artWorksData = [
+    {
+        id: 1,
+        name: 'tribalArt',
+        alternativeText: 'tribal art',
+        caption: 'Rise of the power within us against all odds!',
+        imgSrc: tribalArt,
+    },
+    {
+        id: 2,
+        name: 'dance',
+        alternativeText: 'dance',
+        caption: 'The Graceful Dancer!',
+        imgSrc: dance,
+    },
+    {
+        id: 3,
+        name: 'coffeeGirl',
+        alternativeText: 'coffee girl',
+        caption: 'Enjoying a nice cup of coffee!',
+        imgSrc: coffeeGirl,
+    },
+    {
+        id: 4,
+        name: 'rain',
+        alternativeText: 'rain',
+        caption: 'A walk with the drizzle',
+        imgSrc: rain,
+    },
+    {
+        id: 5,
+        name: 'rocket',
+        alternativeText: 'rocket',
+        caption: 'Let us ride to the moon!',
+        imgSrc: rocket,
+    },
+    {
+        id: 6,
+        name: 'jumpingjack',
+        alternativeText: 'jumping jack',
+        caption: 'Let us create magic moments in our lives!',
+        imgSrc: jumpingjack,
+    },
+    {
+        id: 7,
+        name: 'krishna',
+        alternativeText: 'krishna',
+        caption: 'The mischievous Lord Krishna',
+        imgSrc: krishna,
+    },
+    {
+        id: 8,
+        name: 'sunset',
+        alternativeText: 'sunset',
+        caption: 'A family kissed by the sun while saying goodbye to the day and welcoming the stary night',
+        imgSrc: sunset,
+    },
+    {
+        id: 9,
+        name: 'ride',
+        alternativeText: 'ride',
+        caption: 'Let us take a breath of fresh air through a long ride',
+        imgSrc: ride,
+    }
+]
+
+export default artWorksData;
